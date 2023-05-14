866d0322899ab650c74a9bcff308bfaa7ec5eeb2
Gustavo Alves <gustavoalves.a7@gmail.com>
2023-05-14 01:27:23 -0300

    feat: add error responses and messages. [main]

--- src/Pix.php
+++ src/Pix.php
@@ -3,9 +3,10 @@
 namespace Messer\Pix;
 
 use Messer\Pix\Constants;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
- * Class to mask predefined or custom values
+ * Class to make Pix image and code
  */
 class Pix
 {
@@ -16,48 +17,85 @@ class Pix
 
     public function generate($data)
     {
+        $errorMessage = '';
         $data = json_decode(json_encode($data), false);
 
         $base = $this->pixDecoder($this->getBase());
 
-        if(!empty($data->name))
-        {
-            $base .= $this->pixDecoder(Constants::NAME) . $data->name;
+        if (!empty($data->name)) {
+
+            $base .= $this->pixDecoder(Constants::NAME) . urlencode($data->name);
+
+        } else {
+            
+            $errorMessage =  "The 'name' is required.";
+            throw new HttpException(500, $errorMessage);
+
         }
 
-        if (!empty($data->city))
-        {
-            $base .= $this->pixDecoder(Constants::CITY) . $data->city;
+        if (!empty($data->city)) {
+
+            $base .= $this->pixDecoder(Constants::CITY) . urlencode($data->city);
+
+        } else {
+            
+            $errorMessage =  "The 'city' is required.";
+            throw new HttpException(500, $errorMessage);
+
+        }
+
+        if (!empty($data->key)) {
+
+            $base .= $this->pixDecoder(Constants::KEY) . urlencode($data->key);
+
+        } else {
+
+            $errorMessage =  "The 'key' (chave pix) is required.";
+            throw new HttpException(500, $errorMessage);
+
         }
         
         if (!empty($data->value))
         {
-            $base .= $this->pixDecoder(Constants::VALUE) . $data->value;
+            $base .= $this->pixDecoder(Constants::VALUE) . urlencode($data->value);
         }
 
-        if (!empty($data->key))
+        if (!empty($data->description))
         {
-            $base .= $this->pixDecoder(Constants::KEY) . $data->key;
+            $base .= $this->pixDecoder(Constants::DESCRIPTION) . urlencode($data->description);
+        }
+
+        $destiny = '';
+
+        if(!empty($data->destiny)){
+
+            $destiny = $data->destiny . "/";
+            
+            if (!is_dir($destiny)) {
+                $errorMessage =  "The directory '{$data->destiny}' not exists.";
+                throw new HttpException(500, $errorMessage);
+            }
         }
 
         $response = '';
+        $output = 'string';
 
         if (!empty($data->output))
         {
-            switch ($data->output) {
+            $output = $data->output;
+            switch ($output) {
                 case 'string':
                     $base .= $this->pixDecoder(Constants::OUTPUT_STRING);
                     $response = $this->pixLoadString($base);
-                    dd($response);
                     break;
                 case 'image':
                     $base .= $this->pixDecoder(Constants::OUTPUT_IMAGE);
-                    $response = $this->pixLoadImage($base);
-                    dd($response);
+                    $response = $this->pixLoadImage($base, $destiny);
                     break;
                 
                 default:
-                    return 'erro!';
+                    $errorMessage =  "The output format isn't accepted. Try 'string' or 'image'.";
+                    throw new HttpException(500, $errorMessage);
                     break;
             }
         }
@@ -66,11 +104,13 @@ class Pix
 
     }
 
-    public function pixLoadImage($base)
-    {
-        $image = file_get_contents($base);
-        file_put_contents('messerpix-qrcode.jpg', $image); 
-        return $image;
+    public function pixLoadImage($base, $destiny) {
+        $url = html_entity_decode($base);
+        $image = file_get_contents($url);
+        $unixTimestamp = time();
+        $filename = $destiny . $this->pixDecoder(Constants::SIGNATURE) . $unixTimestamp . $this->pixDecoder(Constants::JPG);
+        file_put_contents($filename, $image);
+        return $filename;
     }
 
     public function pixLoadString($base)
